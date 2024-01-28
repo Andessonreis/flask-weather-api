98a4f6e6393956e2f2e31571522d4de4a1bff765
andesson reis <andessonreys@gmail.com>
2024-01-28 17:31:27 -0300

    :recycle: refactor(app): Update app structure
    - Moved route handling to 'weather_routes.py' and used a Blueprint for better modularity.
    - Updated the main app file ('app.py') to reflect the new structure.

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from app.routes.weather_routes import app
+from utils.data_operations import read_csv, save_to_database
+from routes.weather_routes import weather_blueprint
+from flask import Flask
+
+app = Flask(__name__)
+app.register_blueprint(weather_blueprint, url_prefix='/weather')
 
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
+
+ 
